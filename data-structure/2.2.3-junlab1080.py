fdcfcdd32d37b5e0c7189f43626209d5fcd46368
Mun-Yeol Yoon <gmakin36@gmail.com>
2023-12-19 14:55:48 +0900

    소문자를 대문자로 바꾸기

---
+++ data-structure/2.2.3-junlab1080.py
@@ -0,0 +1,22 @@
+# 소문자를 대문자로 바꾸기
+
+A = input()
+
+B = A.upper()
+
+print(B)
+
+
+
+'''
+# A : 알파벳 대소문자로 구성된 문자열
+# 문자열 A의 모든 소문자를 해당 소문자에 대응하는 대문자로 치환한 문자열을 반환
+
+def solution(A):
+    B = A.upper()
+    return B
+
+A = input()
+print(solution(A))
+
+'''
